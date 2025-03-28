67bbf7ec11d0d026d45c66fb5aa0a797aa148973
Jakub Zilinek <zilinek.jakub@gmail.com>
2025-03-28 13:19:25 +0100

    added time compare test

--- time.sh
+++ time.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 
-echo "Baseline: " > times.txt
+echo "Baseline: " >> times.txt
 { time ./calculate_average_baseline.sh } > result.txt 2>>times.txt
 echo "Go: " >> times.txt
 { time go run 1brc.go } > result_go.txt 2>>times.txt
