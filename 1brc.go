fbef4f8bfc1575da820435fa581bcfb1762cbaa2
Jakub Zilinek <zilinek.jakub@gmail.com>
2025-04-04 13:39:55 +0200

    reworking parsing

--- 1brc.go
+++ 1brc.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"math"
 	"os"
 	"slices"
 	"sort"
-	"strconv"
 	"strings"
 )
 
@@ -16,7 +16,31 @@ func round(x float64) float64 {
 	return math.Floor((x+0.05)*10) / 10
 }
 
-func readFile(filePath string) map[string][]float64 {
+func parseStringToInt(s string) (int64, error) {
+
+	// negative
+	negative := false
+	if len(s) == 5 {
+		s = s[1:5]
+		negative = true
+	}
+
+	var result int64 = 0
+	switch size := len(s); size {
+	case 3:
+		result = int64(s[0])*10 + int64(s[2])
+	case 4:
+		result = int64(s[0])*100 + int64(s[1])*10 + int64(s[3])
+	default:
+		return 0, errors.New("invalid value in input")
+	}
+	if negative {
+		result = result * -1
+	}
+	return result, nil
+}
+
+func readFile(filePath string) map[string][]int64 {
 	file, err := os.Open(filePath)
 	if err != nil {
 		panic(err)
@@ -24,13 +48,13 @@ func readFile(filePath string) map[string][]float64 {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var resultMap = make(map[string][]float64, 10000)
+	var resultMap = make(map[string][]int64, 10000)
 
 	for scanner.Scan() {
 		text := scanner.Text()
 		splitString := strings.SplitN(text, ";", 2)
 		place, value := splitString[0], splitString[1]
-		valueFloat, _ := strconv.ParseFloat(strings.TrimSpace(value), 32)
+		valueFloat, _ := parseStringToInt(value)
 		if _, ok := resultMap[place]; ok {
 			// just update value
 			resultMap[place] = append(resultMap[place], valueFloat)
@@ -38,14 +62,14 @@ func readFile(filePath string) map[string][]float64 {
 			// allocate and update
 			// possible better solution
 			// resultMap[place] = []float64{valueFloat}
-			resultMap[place] = make([]float64, 0, 1)
+			resultMap[place] = make([]int64, 0, 1)
 			resultMap[place] = append(resultMap[place], valueFloat)
 		}
 	}
 	return resultMap
 }
 
-func printResults(result map[string][]float64) {
+func printResults(result map[string][]int64) {
 	// Extract keys and sort them
 	keys := make([]string, 0, len(result))
 	for k := range result {
@@ -57,7 +81,7 @@ func printResults(result map[string][]float64) {
 	// Iterate over sorted keys
 	fmt.Print("{")
 	for i, k := range keys {
-		var sum float64 = 0.0
+		var sum int64 = 0
 		for _, temp := range result[k] {
 			sum += temp
 		}
@@ -75,6 +99,6 @@ func printResults(result map[string][]float64) {
 }
 
 func main() {
-	var resultMap map[string][]float64 = readFile("measurements.txt")
+	var resultMap map[string][]int64 = readFile("measurements.txt")
 	printResults(resultMap)
 }
--- time.sh
+++ time.sh
@@ -2,9 +2,13 @@
 
 
 echo "Baseline: " >> times.txt
-{ time ./calculate_average_baseline.sh } > result.txt 2>>times.txt
+
+{ /opt/homebrew/opt/gnu-time/libexec/gnubin/time -v ./calculate_average_baseline.sh } > result.txt 2>>times.txt
 echo "Go: " >> times.txt
-{ time go run 1brc.go } > result_go.txt 2>>times.txt
+go build 1brc.go
+chmod +x 1brc
+{ /opt/homebrew/opt/gnu-time/libexec/gnubin/time -v ./1brc } > result_go.txt 2>>times.txt
+rm -rf 1brc
 
 cat times.txt
 
